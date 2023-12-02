cf76ce2cc33e9b72d3ea10b78e5bbe4de53fc31a
Simon Karman <mail@simonkarman.nl>
2023-12-02 13:41:43 +0100

    Fixed test result for part 2 of day 2

--- src/day2.rs
+++ src/day2.rs
@@ -78,7 +78,7 @@ mod tests {
     #[test]
     fn sample2() {
         assert_eq!(part2(input_generator("").iter().as_ref()), 0);
-        assert_eq!(part2(input_generator("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\nGame 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\nGame 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\nGame 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\nGame 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green").iter().as_ref()), 0);
+        assert_eq!(part2(input_generator("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\nGame 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\nGame 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\nGame 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\nGame 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green").iter().as_ref()), 2286);
     }
 
 }
