4cb56e8287ad74fdeb10460abaf73120b8674857
Simon Karman <mail@simonkarman.nl>
2023-12-03 10:41:49 +0100

    Implemented part 2 of day 3

--- src/day3.rs
+++ src/day3.rs
@@ -1,12 +1,14 @@
+use std::collections::HashMap;
+
 #[aoc(day3, part1)]
-pub fn part1(_input: &str) -> i32 {
-    let width = _input.lines().next().unwrap().chars().count() as i32;
-    let height = _input.lines().count() as i32;
+pub fn part1(input: &str) -> i32 {
+    let width = input.lines().next().unwrap().chars().count() as i32;
+    let height = input.lines().count() as i32;
     let char_at = |x: i32, y: i32| -> char {
         if x < 0 || x >= width || y < 0 || y >= height {
             return '.';
         }
-        return _input.chars().nth((y * (width + 1) + x) as usize).unwrap();
+        return input.chars().nth((y * (width + 1) + x) as usize).unwrap();
     };
     let is_symbol = |c: char| -> bool {
         return c != '.' && !c.is_digit(10);
@@ -34,8 +36,76 @@ pub fn part1(_input: &str) -> i32 {
 }
 
 #[aoc(day3, part2)]
-pub fn part2(_input: &str) -> i32 {
-    return 0;
+pub fn part2(input: &str) -> i32 {
+    let width = input.lines().next().unwrap().chars().count() as i32;
+    let height = input.lines().count() as i32;
+    let get_index = |x: i32, y: i32| -> i32 { y * (width + 1) + x };
+    let char_at = |x: i32, y: i32| -> char {
+        if x < 0 || x >= width || y < 0 || y >= height {
+            return '.';
+        }
+        return input.chars().nth(get_index(x, y) as usize).unwrap();
+    };
+    let is_gear = |c: char| -> bool {
+        return c == '*';
+    };
+    let mut gears: HashMap<i32, (i32, i32)> = HashMap::from([]);
+    for y in 0..height {
+        let mut x = 0;
+        while x < width {
+            let mut adjacent_gears: Vec<i32> = vec![];
+            let mut digits = "".to_owned();
+
+            // Find gears on left
+            if is_gear(char_at(x - 1, y - 1)) {
+                adjacent_gears.push(get_index(x - 1, y - 1))
+            }
+            if is_gear(char_at(x - 1, y)) {
+                adjacent_gears.push(get_index(x - 1, y))
+            }
+            if is_gear(char_at(x - 1, y + 1)) {
+                adjacent_gears.push(get_index(x - 1, y + 1))
+            }
+
+            while char_at(x, y).is_digit(10) {
+                digits += char_at(x, y).to_string().as_str();
+                // find gears middle
+                if is_gear(char_at(x, y - 1)) {
+                    adjacent_gears.push(get_index(x, y - 1))
+                }
+                if is_gear(char_at(x, y + 1)) {
+                    adjacent_gears.push(get_index(x, y + 1))
+                }
+                x += 1;
+            }
+
+            // find gears on right
+            if is_gear(char_at(x, y - 1)) {
+                adjacent_gears.push(get_index(x, y - 1))
+            }
+            if is_gear(char_at(x, y)) {
+                adjacent_gears.push(get_index(x, y))
+            }
+            if is_gear(char_at(x, y + 1)) {
+                adjacent_gears.push(get_index(x, y + 1))
+            }
+
+            let number = digits.parse().unwrap_or(0);
+            if number > 0 {
+                adjacent_gears.iter().for_each(|gear| {
+                    let (count, product) = gears.get(gear).unwrap_or(&(0, 1));
+                    gears.insert(*gear, (count + 1, product * number));
+                });
+            }
+            x += 1;
+        }
+    }
+    return gears.iter().fold(0, |acc, (_gear_id, (count, product))| {
+        if *count == 2 {
+            return acc + *product;
+        }
+        return acc;
+    });
 }
 
 #[cfg(test)]
@@ -51,7 +121,7 @@ mod tests {
     // part 2
     #[test]
     fn sample2() {
-        assert_eq!(part2(""), 0);
+        assert_eq!(part2("467..114..\n...*......\n..35..633.\n......#...\n617*......\n.....+.58.\n..592.....\n......755.\n...$.*....\n.664.598..\n"), 467835);
     }
 
 }
