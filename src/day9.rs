67a93ed217a934f32ea05b633e68c485da441b69
Simon Karman <mail@simonkarman.nl>
2023-12-09 11:16:18 +0100

    Implemented part 2 of day 9

--- src/day9.rs
+++ src/day9.rs
@@ -18,6 +18,16 @@ impl History {
         }
         return (History { data: derivatives }).predict() + last;
     }
+
+    pub fn predict_back(&self) -> i32 {
+        let derivatives: Vec<i32> = vec![0; self.data.len() - 1].iter().enumerate()
+            .map(|(index, _)| self.data[index + 1] - self.data[index]).collect();
+        let first =  *self.data.first().unwrap();
+        if derivatives.iter().all(|v| *v == 0) {
+            return first;
+        }
+        return first - (History { data: derivatives }).predict_back();
+    }
 }
 
 #[aoc(day9, part1)]
@@ -26,8 +36,8 @@ pub fn part1(lines: &str) -> i32 {
 }
 
 #[aoc(day9, part2)]
-pub fn part2(_input: &str) -> i32 {
-    return 0;
+pub fn part2(lines: &str) -> i32 {
+    return lines.lines().map(History::new).map(|history| history.predict_back()).sum();
 }
 
 #[cfg(test)]
@@ -43,7 +53,7 @@ mod tests {
     // part 2
     #[test]
     fn sample2() {
-        assert_eq!(part2(""), 0);
+        assert_eq!(part2("0 3 6 9 12 15\n1 3 6 10 15 21\n10 13 16 21 30 45"), 2);
     }
 
 }
