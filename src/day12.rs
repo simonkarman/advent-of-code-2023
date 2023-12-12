7276cd97fbdec98f48024430f37cf7557e5bbc83
Simon Karman <mail@simonkarman.nl>
2023-12-12 20:24:19 +0100

    Implemented part 1 of day 12

---
+++ src/day12.rs
@@ -0,0 +1,124 @@
+fn can_place_at(row: &str, index: usize, size: usize, exhaustive: bool) -> bool {
+    // length must be sufficient
+    let length = row.chars().count();
+    if index + size > length { return false; }
+
+    // previous must be a possible boundary or begin
+    if index > 0 {
+        let previous = row.chars().nth(index - 1).unwrap();
+        if previous == '#' { return false; }
+    }
+
+    // next must be a possible boundary or end
+    let next = row.chars().nth(index + size).unwrap_or('.');
+    if next == '#' { return false; }
+
+    // all in between must be possible
+    if !row.chars().skip(index).take(size).all(|v| v == '?' || v == '#') {
+        return false;
+    }
+
+    // ensure that the string has been exhausted (all #'s have been used)
+    if exhaustive {
+        return row.chars().skip(index + size).all(|v| v != '#');
+    }
+
+    // finally, this placement is valid
+    return true;
+}
+
+fn find_placements(row: &str, size: usize, exhaustive: bool) -> Vec<usize> {
+    let mut found = vec![];
+    for index in 0..row.chars().count() {
+        if can_place_at(row, index, size, exhaustive) {
+            found.push(index);
+        }
+        // Stop after we have started with a hashtag,
+        //  as moving further would mean leaving a hashtag behind
+        if row.chars().nth(index).unwrap() == '#' {
+            break;
+        }
+    }
+    return found;
+}
+
+fn find_arrangements(row: &str, group_sizes: &Vec<usize>) -> usize {
+    assert_ne!(group_sizes.len(), 0);
+
+    // If this is the last group placement, then return the number of possible exhaustive placements
+    if group_sizes.len() == 1 {
+        return find_placements(row, group_sizes[0], true).len();
+    }
+
+    // Otherwise recursively continue with the remaining row and groups from the different placements
+    return find_placements(row, group_sizes[0], false).iter()
+        .map(|placement| {
+            let continue_from = *placement + group_sizes[0] + 1;
+            if continue_from >= row.len() {
+                return 0;
+            }
+            return find_arrangements(
+                &row[continue_from..],
+                &group_sizes.iter().map(|v| *v).skip(1).collect(),
+            )
+        })
+        .sum();
+}
+
+#[aoc(day12, part1)]
+pub fn part1(input: &str) -> usize {
+    return input.lines().map(|line| find_arrangements(
+        line.split(' ').next().unwrap(),
+        &line.split(' ').skip(1).next().unwrap().split(',').map(|v| v.parse().unwrap()).collect()
+    )).sum();
+}
+#[aoc(day12, part2)]
+pub fn part2(_input: &str) -> usize {
+    return 0;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{find_placements, find_arrangements, part1, part2};
+
+    #[test]
+    fn find_placements_tests() {
+        assert_eq!(find_placements("???", 1, false), vec![0, 1, 2]);
+        assert_eq!(find_placements("?#??", 1, false), vec![1]);
+        assert_eq!(find_placements("???", 2, false), vec![0, 1]);
+        assert_eq!(find_placements("??.???", 4, false), vec![]);
+        assert_eq!(find_placements("???..?#?#??", 3, false), vec![0, 6]);
+        assert_eq!(find_placements("???..???##??", 2, false), vec![0, 1, 5, 8]);
+        assert_eq!(find_placements("???..?#?#???", 2, false), vec![0, 1, 5]);
+        assert_eq!(find_placements("?#?", 1, false), vec![1]);
+        assert_eq!(find_placements("?#", 1, false), vec![1]);
+        assert_eq!(find_placements("#?", 1, false), vec![0]);
+        assert_eq!(find_placements("?????#?????", 4, false), vec![0, 2, 3, 4, 5]);
+
+
+        assert_eq!(find_placements("???", 1, true), vec![0, 1, 2]);
+        assert_eq!(find_placements("#??", 1, true), vec![0]);
+        assert_eq!(find_placements("?????#?????", 4, true), vec![2, 3, 4, 5]);
+        assert_eq!(find_placements("?#???#?????", 4, true), vec![]);
+    }
+
+    #[test]
+    fn find_arrangements_tests() {
+        assert_eq!(find_arrangements("???.###", &vec![1, 1, 3]), 1);
+        assert_eq!(find_arrangements("????", &vec![2, 1]), 1);
+        assert_eq!(find_arrangements("?###????????", &vec![3, 2, 1]), 10);
+    }
+
+    #[test]
+    fn samples() {
+        let example = "???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1";
+        assert_eq!(part1(example), 21);
+        assert_eq!(part2(example), 0);
+    }
+
+}
