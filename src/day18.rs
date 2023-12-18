9156320f4a269cc6c8ebf12ef47ee014fbcf5a3e
Simon Karman <mail@simonkarman.nl>
2023-12-18 13:44:26 +0100

    Implemented part 2 of day 18

--- src/day18.rs
+++ src/day18.rs
@@ -1,6 +1,6 @@
 use std::cmp;
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 pub enum Direction {
     Up,
     Right,
@@ -8,32 +8,10 @@ pub enum Direction {
     Left,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 pub struct Instruction {
     direction: Direction,
     meters: usize,
-    _color: String,
-}
-
-#[aoc_generator(day18)]
-pub fn input_generator(input: &str) -> Vec<Instruction> {
-    input
-        .lines()
-        .map(|l| {
-            let splits: Vec<&str> = l.split(' ').collect();
-            let color = splits.get(2).unwrap();
-            Instruction {
-                direction: match *splits.get(0).unwrap() {
-                    "U" => Direction::Up,
-                    "R" => Direction::Right,
-                    "D" => Direction::Down,
-                    "L" => Direction::Left,
-                    _ => panic!("unknown direction {}", splits.get(0).unwrap()),
-                },
-                meters: splits.get(1).unwrap().parse().unwrap(),
-                _color: color[2..(color.len() - 1)].to_string(),
-            }
-        }).collect()
 }
 
 fn get_dimensions(instructions: &Vec<Instruction>) -> (/*width*/ usize, /*height*/ usize, /*start_x*/ usize, /*start_y*/ usize) {
@@ -70,10 +48,24 @@ fn get_diff(direction: &Direction) -> (i32, i32) {
 }
 
 #[aoc(day18, part1)]
-pub fn part1(instructions: &Vec<Instruction>) -> usize {
-    println!("{:?}", instructions);
+pub fn part1(input: &str) -> usize {
+    let instructions = &input
+        .lines()
+        .map(|l| {
+            let splits: Vec<&str> = l.split(' ').collect();
+            Instruction {
+                direction: match *splits.get(0).unwrap() {
+                    "U" => Direction::Up,
+                    "R" => Direction::Right,
+                    "D" => Direction::Down,
+                    "L" => Direction::Left,
+                    _ => panic!("unknown direction {}", splits.get(0).unwrap()),
+                },
+                meters: splits.get(1).unwrap().parse().unwrap(),
+            }
+        }).collect();
+
     let (width, height, start_x, start_y) = get_dimensions(instructions);
-    println!("dimensions: {}w x {}h (start at {},{})", width, height, start_x, start_y);
     let mut holes = vec![false; width * height];
     let mut x = start_x;
     let mut y = start_y;
@@ -105,21 +97,55 @@ pub fn part1(instructions: &Vec<Instruction>) -> usize {
         if y > 0 { try_visit(x, y - 1) };
         if y < height - 1 { try_visit(x, y + 1) };
     }
-    holes.chunks(width).enumerate().for_each(|(y, line)| println!(
-        "{}",
-        line.iter().enumerate().map(|(x, v)| if x == start_x && y == start_y { 'S' } else if *v { '#' } else { '.' } ).collect::<String>()
-    ));
     return holes.iter().filter(|h| **h).count();
 }
 
 #[aoc(day18, part2)]
-pub fn part2(_instructions: &Vec<Instruction>) -> usize {
-    return 0;
+pub fn part2(input: &str) -> f64 {
+    let instructions: Vec<Instruction> = input.lines().map(|l| {
+        let splits: Vec<&str> = l.split(' ').collect();
+        let color = splits.get(2).unwrap();
+        Instruction {
+            direction: match color.chars().nth(color.len() - 2).unwrap() {
+                '0' => Direction::Right,
+                '1' => Direction::Down,
+                '2' => Direction::Left,
+                '3' => Direction::Up,
+                _ => panic!("unknown direction {}", color.chars().nth(color.len() - 2).unwrap()),
+            },
+            meters: usize::from_str_radix(&color[2..(color.len() - 2)], 16).unwrap(),
+        }
+    }).collect();
+    let mut x = 0;
+    let mut y = 0;
+    let mut points: Vec<(f64, f64)> = vec![(x as f64, y as f64)];
+    instructions.iter().for_each(|instruction| {
+        let (x_diff, y_diff) = get_diff(&instruction.direction);
+        x += x_diff * (instruction.meters as i32);
+        y += y_diff * (instruction.meters as i32);
+        points.push((x as f64, y as f64));
+    });
+    assert_eq!(points[0], points[points.len() - 1]);
+
+    // Calculate area of a polygon
+    let area = 0.5f64 * (0..(points.len() - 1)).map(|i| {
+        let (ax, ay) = points[i];
+        let (bx, by) = points[i + 1];
+        return ax * by - bx * ay;
+    }).sum::<f64>();
+    let circumference = 1f64 + (0..(points.len() - 1)).map(|i| {
+        let (ax, ay) = points[i];
+        let (bx, by) = points[i + 1];
+        let x_diff = ax - bx;
+        let y_diff = ay - by;
+        return (if x_diff < 0f64 {-x_diff} else {x_diff}) + (if y_diff < 0f64 {-y_diff} else {y_diff});
+    }).sum::<f64>() / 2f64;
+    return area + circumference;
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{input_generator, part1, part2};
+    use super::{part1, part2};
 
     #[test]
     fn samples() {
@@ -137,7 +163,7 @@ R 2 (#7807d2)
 U 3 (#a77fa3)
 L 2 (#015232)
 U 2 (#7a21e3)";
-        assert_eq!(part1(&input_generator(example)), 62);
-        assert_eq!(part2(&input_generator(example)), 0);
+        assert_eq!(part1(example), 62);
+        assert_eq!(part2(example), 952408144115f64);
     }
 }
