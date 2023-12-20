c7ddff6c1dfc4101ed905a769d291df12bbb6e89
Simon Karman <mail@simonkarman.nl>
2023-12-20 20:20:16 +0100

    Implemented part 2 of day 20

--- src/day20.rs
+++ src/day20.rs
@@ -56,13 +56,44 @@ pub fn part1(input: &str) -> usize {
             }
         })
     });
+
+    // Draw graphviz
+    println!("--graphviz--\ndigraph G {{  broadcaster -> hd,xt,kj,zt;\n  broadcaster [color=green];\n  rx [color=green];\n");
+    modules.iter().for_each(|(k, v)| {
+        let color = match v.variant {
+            Variant::FlipFlop { .. } => "black",
+            Variant::Conjunction => "blue",
+        };
+        println!("  {} [color={}];{} -> {};", k, color, k, v.targets.join(","));
+    });
+    println!("}}\n--end graphviz--");
+    println!();
+
+    // Clusters
+    // xr: *hd,tp,rq,gr,zn,fh,xs,*sb,jh,bz,rg,dl
+    // tf: *xt,?cc,cr,?kk,fc,qx,?gq,jm,pj,dk,tb,kr
+    // gk: *zt,?xg,rd,cz,?vv,?br,fm,?dj,pd,kb,nt,ph
+    // gx: *kj,hs,?tk,?tt,gf,?sh,lr,?mz,rv,ck,nf,gd
+    let mut clusters = HashMap::new();
+    clusters.insert("xr", vec!["hd","tp","rq","gr","zn","fh","xs","sb","jh","bz","rg","dl"]);
+    clusters.insert("tf", vec!["xt","cc","cr","kk","fc","qx","gq","jm","pj","dk","tb","kr"]);
+    clusters.insert("gk", vec!["zt","xg","rd","cz","vv","br","fm","dj","pd","kb","nt","ph"]);
+    clusters.insert("gx", vec!["kj","hs","tk","tt","gf","sh","lr","mz","rv","ck","nf","gd"]);
+    clusters.iter_mut().for_each(|(_, parts)| parts.reverse());
+    let mut last_cluster_low_output = HashMap::new();
+
+    // Find pulses
     let mut high_pulses_seen = 0;
     let mut low_pulses_seen = 0;
-    for i in 0..1000 {
+    for i in 0..5000 {
         let mut pulses = pulses.clone();
         low_pulses_seen += 1;
+        let mut last_pulse_from = HashMap::new();
+        let mut has_outputted_low = HashMap::new();
         while let Some((pulse_in, to, from)) = pulses.pop_front() {
-            if pulse_in { high_pulses_seen += 1; } else { low_pulses_seen += 1; }
+            if i < 1000 {
+                if pulse_in { high_pulses_seen += 1; } else { low_pulses_seen += 1; }
+            }
             let module = modules.get(to.as_str());
             if module.is_none() {
                 continue;
@@ -84,12 +115,65 @@ pub fn part1(input: &str) -> usize {
                 Variant::Conjunction => { Some(!module.inputs.values().all(|pulse| *pulse)) }
             };
             if pulse_out.is_none() {
+                last_pulse_from.remove(to.as_str());
                 continue;
+            }
+            last_pulse_from.insert(to.clone(), pulse_out.unwrap());
+            if !pulse_out.unwrap() {
+                has_outputted_low.insert(to.clone(), true);
             }
             for next_target in module.targets.clone() {
                 pulses.push_back((pulse_out.unwrap(), next_target, to.clone()));
             }
             modules.insert(to, module);
+        }
+
+        // visualize clusters
+        // clusters.iter().for_each(|(cluster, parts)| {
+        //     println!("{}: {}", cluster, parts.iter().map(|part| {
+        //         if let Some(val) = last_pulse_from.get(*part) {
+        //             if !*val {
+        //                 return "1";
+        //             };
+        //             return "0";
+        //         }
+        //         return "_";
+        //     }).collect::<String>());
+        // });
+        clusters.iter().for_each(|(cluster, parts)| {
+            if has_outputted_low.get(*cluster).is_none() {
+                return;
+            }
+            let cluster_module = modules.get(*cluster);
+            if cluster_module.is_none() {
+                return;
+            }
+            let cluster_module = cluster_module.unwrap();
+            let inputs = cluster_module.inputs.clone();
+            let diff = last_cluster_low_output.get(*cluster).map(|last| i - last);
+            println!("{}: {} ({:?} from {} since last {:?})", cluster, parts.iter().filter_map(|v| {
+                let i = inputs.get(*v);
+                if i.is_none() {
+                    return None;
+                }
+                return Some(if *i.unwrap() {"1"} else {"0"});
+            }).collect::<String>(), has_outputted_low.get(*cluster), i, diff);
+            last_cluster_low_output.insert(*cluster, i);
+        });
+
+        // visualize cluster outputs
+        clusters.iter().for_each(|(cluster, _)| {
+            if let Some(val) = last_pulse_from.get(*cluster) {
+                if !*val { println!("{}: {}", cluster, i); }
+            }
+        });
+        if clusters.iter().all(|(cluster, _)| {
+            if let Some(val) = last_pulse_from.get(*cluster) {
+                return !*val;
+            }
+            return false;
+        }) {
+            println!("clusters: all found at {}", i);
         }
     }
     return low_pulses_seen * high_pulses_seen;
@@ -97,7 +181,20 @@ pub fn part1(input: &str) -> usize {
 
 #[aoc(day20, part2)]
 pub fn part2(_input: &str) -> usize {
-    return 0;
+    // solution -- see graphviz from part 1
+
+    // graph clusters
+    // xr: *hd,tp,rq,gr,zn,fh,xs,*sb,jh,bz,rg,dl
+    // tf: *xt,?cc,cr,?kk,fc,qx,?gq,jm,pj,dk,tb,kr
+    // gk: *zt,?xg,rd,cz,?vv,?br,fm,?dj,pd,kb,nt,ph
+    // gx: *kj,hs,?tk,?tt,gf,?sh,lr,?mz,rv,ck,nf,gd
+
+    // answers
+    // each cluster cycles in the below intervals
+    return 4021 * 3917 * 3923 * 3967;
+    // too low:        479001600
+    // correct:  245114020323037
+    // too high: 281474980000000
 }
 
 #[cfg(test)]
@@ -118,6 +215,6 @@ mod tests {
 &con -> output";
         assert_eq!(part1(example1), 32000000);
         assert_eq!(part1(example2), 11687500);
-        assert_eq!(part2(example1), 0);
+        assert_eq!(part2(example1), 245114020323037);
     }
 }
