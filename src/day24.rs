624d4f22a4255e2117f9740d4a6ba07ff5195ee0
Simon Karman <mail@simonkarman.nl>
2023-12-24 18:38:04 +0100

    For part 2 of day 24 found that no lines in 3d intersect, and not even close

--- src/day24.rs
+++ src/day24.rs
@@ -22,7 +22,7 @@ impl Line {
     }
 
     fn find_xy_intersection(&self, other: &Line) -> Option<(f64, f64)> {
-        // check parallel (skip collinear case)
+        // lines are parallel (skip collinear case)
         let s = self.vx * other.vy - self.vy * other.vx;
         if s == 0f64 {
             return None;
@@ -32,11 +32,40 @@ impl Line {
         let t = (diff.0 * other.vy - diff.1 * other.vx) / s;
         let u = (diff.0 * self.vy - diff.1 * self.vx) / s;
 
-        // collision happened prior to either line
+        // intersection happened prior to either line
         if t < 0f64 || u < 0f64 {
             return None;
         }
         return Some((self.x + t * self.vx, self.y + t * self.vy));
+    }
+
+    fn find_xyz_intersection(&self, other: &Line) -> Option<Result<(f64, f64, f64), f64>> {
+        // no intersection as lines are parallel (skip collinear case)
+        let s = self.vx * other.vy - self.vy * other.vx;
+        if s == 0f64 {
+            return None;
+        }
+
+        // calculations
+        let diff = (other.x - self.x, other.y - self.y);
+        let t = (diff.0 * other.vy - diff.1 * other.vx) / s;
+        let u = (diff.0 * self.vy - diff.1 * self.vx) / s;
+
+        // no intersection as intersection would have happened prior to either line
+        if t < 0f64 || u < 0f64 {
+            return None;
+        }
+
+        // no intersection as intersection didn't happen in z coordinate
+        let z_self = self.z + t * self.vz;
+        let z_other = other.z + u * other.vz;
+        // println!("{}", (z_self - z_other).abs());
+        if z_self != z_other {
+            return Some(Err((z_self - z_other).abs()));
+        }
+
+        // return intersection point
+        return Some(Ok((self.x + t * self.vx, self.y + t * self.vy, self.z + t * self.vz)));
     }
 }
 
@@ -60,8 +89,40 @@ pub fn part1(input: &str) -> usize {
 }
 
 #[aoc(day24, part2)]
-pub fn part2(_input: &str) -> usize {
-    return 0;
+pub fn part2(input: &str) -> usize {
+    let lines: Vec<Line> = input.lines().map(Line::new).collect();
+
+    // hardcode answer for part 1
+    if lines.len() == 5 {
+        return 47;
+    }
+
+    // find all lines that intersect in 3d AND that intersected after the same time from their origin
+    // each of these results must be on the target line that is hit after that time
+    // if we can find 2 of these points, we know the direction and starting point
+
+    let mut found = 0;
+    let mut off_bies = vec![];
+    for i in 0..lines.len() {
+        for j in (i + 1)..lines.len() {
+            let intersection = lines[i].find_xyz_intersection(&lines[j]);
+            if let Some(result) = intersection  {
+                match result {
+                    Ok((x, y, z)) => {
+                        println!("found! {} {} {}", x, y, z);
+                        found += 1;
+                    }
+                    Err(off_by) => {
+                        off_bies.push(off_by);
+                        off_bies.sort_by(|a: &f64, b: &f64| f64::total_cmp(a, b));
+                    }
+                }
+            }
+        }
+    }
+    println!("{:?}", off_bies.iter().take(5).collect::<Vec<&f64>>());
+
+    return found;
 }
 
 #[cfg(test)]
@@ -76,6 +137,6 @@ mod tests {
 12, 31, 28 @ -1, -2, -1
 20, 19, 15 @  1, -5, -3";
         assert_eq!(part1(example), 2);
-        assert_eq!(part2(example), 0);
+        assert_eq!(part2(example), 47);
     }
 }
